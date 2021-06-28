4288d04e8f92c9e6970d644ecc3ad189b0b52f03
Seksit Seeton <gnah98@hotmail.co.th>
2021-06-28 19:41:22 +0700

    first commit

---
+++ Assets/Editor/ShadowCastersGeneratorEditor.cs
@@ -0,0 +1,31 @@
+
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ShadowCaster2DTileMap))]
+public class ShadowCastersGeneratorEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        ShadowCaster2DTileMap generator = (ShadowCaster2DTileMap)target;
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Destroy All Children"))
+        {
+
+            generator.DestroyAllChildren();
+
+        }
+        EditorGUILayout.Space();
+        if (GUILayout.Button("Generate"))
+        {
+
+            generator.Generate();
+
+        }
+    }
+
+}
---
+++ Assets/ShadowCaster2DTileMap.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+[RequireComponent(typeof(CompositeCollider2D))]
+public class ShadowCaster2DTileMap : MonoBehaviour
+{
+
+    [Space]
+    [SerializeField]
+    private bool selfShadows = true;
+
+    private CompositeCollider2D tilemapCollider;
+
+
+    static readonly FieldInfo meshField = typeof(ShadowCaster2D).GetField("m_Mesh", BindingFlags.NonPublic | BindingFlags.Instance);
+    static readonly FieldInfo shapePathField = typeof(ShadowCaster2D).GetField("m_ShapePath", BindingFlags.NonPublic | BindingFlags.Instance);
+    static readonly MethodInfo generateShadowMeshMethod = typeof(ShadowCaster2D)
+                                    .Assembly
+                                    .GetType("UnityEngine.Experimental.Rendering.Universal.ShadowUtility")
+                                    .GetMethod("GenerateShadowMesh", BindingFlags.Public | BindingFlags.Static);
+    public void Generate()
+    {
+
+        tilemapCollider = GetComponent<CompositeCollider2D>();
+
+        for (int i = 0; i < tilemapCollider.pathCount; i++)
+        {
+            Vector2[] pathVertices = new Vector2[tilemapCollider.GetPathPointCount(i)];
+            tilemapCollider.GetPath(i, pathVertices);
+            GameObject shadowCaster = new GameObject("shadow_caster_" + i);
+            shadowCaster.transform.parent = gameObject.transform;
+            ShadowCaster2D shadowCasterComponent = shadowCaster.AddComponent<ShadowCaster2D>();
+            shadowCasterComponent.selfShadows = this.selfShadows;
+
+            Vector3[] testPath = new Vector3[pathVertices.Length];
+            for (int j = 0; j < pathVertices.Length; j++)
+            {
+                testPath[j] = pathVertices[j];
+            }
+
+            shapePathField.SetValue(shadowCasterComponent, testPath);
+            meshField.SetValue(shadowCasterComponent, new Mesh());
+            generateShadowMeshMethod.Invoke(shadowCasterComponent, new object[] { meshField.GetValue(shadowCasterComponent), shapePathField.GetValue(shadowCasterComponent) });
+        }
+
+        Debug.Log("Generate");
+
+    }
+    public void DestroyAllChildren()
+    {
+
+        var tempList = transform.Cast<Transform>().ToList();
+        foreach (var child in tempList)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+
+    }
+
+}
